df1ea8b3836c6e2e5bcaf647b1606c669f2a7820
lukas <l.misaga@gmail.com>
2021-11-30 17:57:56 +0100

    log4j logger in aspect

--- aspects/src/main/java/org/annoscheme/aspects/AnnotationInterceptor.java
+++ aspects/src/main/java/org/annoscheme/aspects/AnnotationInterceptor.java
@@ -1,5 +1,6 @@
 package org.annoscheme.aspects;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,9 +8,11 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class AnnotationInterceptor {
 
+	private static final Logger logger = Logger.getLogger(AnnotationInterceptor.class);
+
 	@Around("@annotation(org.annoscheme.common.annotation.Action)")
 	public Object annotation(ProceedingJoinPoint joinPoint) throws Throwable {
-		System.out.println(joinPoint.getSignature());
+		logger.info(joinPoint);
 		return joinPoint.proceed();
 	}
 }
